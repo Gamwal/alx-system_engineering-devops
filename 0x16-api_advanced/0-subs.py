35fd9d763a652b3c34875080d648326ac9a1ee06
Gamaliel Adun <adungamaliel@gmail.com>
2023-08-09 17:01:36 +0000

    updated 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-""""""
+"""
+function that queries the Reddit API
+and returns the number of subscribers
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """"""
+    """
+    Function that queries a given subreddit
+
+    Args:
+        subreddit (str): The subreddit to be queried
+
+    Returns:
+        int: The number of total subscriber to the subreddit
+    """
     base_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(base_url)
     no_of_subscribers = response.json().get('data').get('subscribers')
-    print(no_of_subscribers)
     return no_of_subscribers
 
 
