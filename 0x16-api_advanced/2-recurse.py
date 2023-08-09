9f84454693930281cbe0310e78067bcc3e39af04
Gamaliel Adun <adungamaliel@gmail.com>
2023-08-09 21:13:04 +0000

    added 2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries the Reddit
+API and returns a list containing the titles
+of all hot articles for a given subreddit
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Function that queries a subreddit
+
+    Args:
+        subreddit (str): The subreddit to be queried
+        hot_list (list): list to store titles
+
+    Returns:
+        list: List of all hot topics in subreddit
+              None if not a valid subreddit
+    """
+
+    base_url = "https://www.reddit.com/r/{}/hot.json"\
+        .format(subreddit)
+    headers = {'User-agent': 'yourbot'}
+    params = {'after': after} if after else {}
+    response = requests.get(base_url, headers=headers,
+                            params=params, allow_redirects=False)
+
+    if response.status_code == 200:
+        response_list = response.json().get('data').get('children')
+        hot_list.extend([i.get('data').get('title') for i in response_list])
+
+        after = response.json().get('data').get('after')
+
+        if after:
+            return recurse(subreddit, hot_list, after)
+        else:
+            return hot_list
+    else:
+        return None
+
+
+if __name__ == '__main__':
+    recurse(subreddit=None)
